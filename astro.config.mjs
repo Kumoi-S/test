0ba7b2f0c9576f7269e1a1de724b984289666390
smy 4070laptop <kumoishizuka@gmail.com>
2025-02-01 13:22:46 +0800

    .

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://kumoi-s.github.io',
+    base: '/test',
+});
